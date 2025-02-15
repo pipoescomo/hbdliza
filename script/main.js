df3c92e0c13ada0d63962dd8f158a0d66bf9a855
Emilio Escobedo <emilio@tiigo.us>
2025-02-15 08:40:07 -0800

    liza changes

--- script/main.js
+++ script/main.js
@@ -1,22 +1,22 @@
 // trigger to play music in the background with sweetalert
-window.addEventListener('load', () => {
-    Swal.fire({
-        title: 'Do you want to play music in the background?',
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes',
-        cancelButtonText: 'No',
-    }).then((result) => {
-        if (result.isConfirmed) {
-            document.querySelector('.song').play();
-            animationTimeline();
-        } else {
-            animationTimeline();
-        }
-    });
-});
+// window.addEventListener('load', () => {
+//     Swal.fire({
+//         title: 'Do you want to play music in the background?',
+//         icon: 'warning',
+//         showCancelButton: true,
+//         confirmButtonColor: '#3085d6',
+//         cancelButtonColor: '#d33',
+//         confirmButtonText: 'Yes',
+//         cancelButtonText: 'No',
+//     }).then((result) => {
+//         if (result.isConfirmed) {
+//             document.querySelector('.song').play();
+//             animationTimeline();
+//         } else {
+//             animationTimeline();
+//         }
+//     });
+// });
 
 
 // animation timeline
@@ -270,3 +270,5 @@ const animationTimeline = () => {
         tl.restart();
     });
 }
+
+animationTimeline();
